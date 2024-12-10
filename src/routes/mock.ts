c78e443d6d98532bb0ff38783299bab5d07e5c5c
Hyeanwoo <wkdus7h@naver.com>
2024-12-10 18:52:46 +0900

    feat: add mock router

---
+++ src/routes/mock.ts
@@ -0,0 +1,18 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { mockingPosition } from '../utils';
+
+export default async function (server: FastifyInstance) {
+  // server.get(
+  //   '/mock-arrival',
+  //   async (request: FastifyRequest, reply: FastifyReply) => {
+  //     return {};
+  //   },
+  // );
+
+  server.get(
+    '/mock-position',
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      reply.send(mockingPosition('line4'));
+    },
+  );
+}
