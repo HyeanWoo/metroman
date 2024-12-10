9abc35dc2530a07913007cf9a9734425b89211ba
Hyeanwoo <wkdus7h@naver.com>
2024-12-10 19:03:42 +0900

    refactor: detach healthCheck router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
-import { FastifyInstance } from 'fastify';
+import type { FastifyInstance } from 'fastify';
 import mockHandler from './mock';
+import healthCheckHandler from './healthCheck';
 
 export default function (server: FastifyInstance) {
+  healthCheckHandler(server);
   mockHandler(server);
 }
--- src/routes/mock.ts
+++ src/routes/mock.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import type { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { mockingPosition } from '../utils';
 
 export default async function (server: FastifyInstance) {
