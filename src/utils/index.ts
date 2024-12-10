1afc3a40cf772b2f8bbc664edd61afd3d7996899
Hyeanwoo <wkdus7h@naver.com>
2024-12-10 18:51:34 +0900

    feat: create metro position mocking function

---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+export type {
+  MetroLineType,
+  MetroLineNameKorType,
+  MetroLineKeyType,
+} from './constants';
+export { MetroLine, MetroLineNameKor, StationList } from './constants';
+
+export { mockingPosition } from './mocking';
---
+++ src/utils/mocking.ts
@@ -0,0 +1,65 @@
+import {
+  MetroLine,
+  MetroLineKeyType,
+  MetroLineNameKor,
+  StationList,
+} from './constants';
+
+function addRandomTime(currentTime: Date): Date {
+  const randomMs = Math.floor(Math.random() * (3 * 60 * 1000)) + 1 * 60 * 1000;
+  const futureTime = new Date(currentTime.getTime() + randomMs);
+  return futureTime;
+}
+
+function getMetroDate(date: Date): string {
+  return `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}`;
+}
+
+function getFullMetroTime(date: Date): string {
+  return date.toISOString().replace('T', ' ').split('.')[0];
+}
+
+export function mockingPosition(lineNumber: MetroLineKeyType) {
+  const min = 30;
+  const max = 40;
+  const totalMetroInService = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  const realtimePositionList = [];
+
+  let currentDate = new Date();
+  for (let i = 0; i < totalMetroInService; i++) {
+    const randomStation = StationList[Math.floor(Math.random() * 48)];
+    const recentDate = getMetroDate(currentDate);
+    const recentTime = getFullMetroTime(currentDate);
+    const updnLine = Math.floor(Math.random() * 2);
+
+    const oneMetro = {
+      rowNum: i + 1,
+      selectedCount: totalMetroInService,
+      subwayId: MetroLine[lineNumber],
+      subwayNm: MetroLineNameKor[lineNumber],
+      statnId: randomStation.id.toString(),
+      statnNm: randomStation.name,
+      trainNo: Math.floor(Math.random() * totalMetroInService) + 1,
+      lastRecptnDt: recentDate,
+      recptnDt: recentTime,
+      updnLine: updnLine.toString(),
+      statnTid: updnLine === 0 ? '1004000409' : '1004000456',
+      statnTnm: updnLine === 0 ? '당고개' : '오이도',
+      trainSttus: '1',
+      directAt: '0',
+      lstcarAt: '0',
+    };
+
+    realtimePositionList.push(oneMetro);
+    currentDate = addRandomTime(currentDate);
+  }
+
+  return {
+    errorMessage: {
+      status: 200,
+      total: totalMetroInService,
+    },
+    realtimePositionList,
+  };
+}
