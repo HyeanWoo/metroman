6a170f12203b3500387e8cca3829e527ce45c2d9
Hyeanwoo <wkdus7h@naver.com>
2024-12-11 17:35:08 +0900

    feat: add new types for metro-line

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,3 +11,38 @@ import {
 export type MetroLineKeyType = keyof typeof MetroLine;
 export type MetroLineType = (typeof MetroLine)[MetroLineKeyType];
 export type MetroLineNameKorType = (typeof MetroLineNameKor)[MetroLineKeyType];
+
+export type UpDownLineType = (typeof UpDownLine)[keyof typeof UpDownLine];
+export type TrainStateStatusType =
+  (typeof TrainStateStatus)[keyof typeof TrainStateStatus];
+export type ExpressStatusType =
+  (typeof ExpressStatus)[keyof typeof ExpressStatus];
+export type LastStatusType = (typeof LastStatus)[keyof typeof LastStatus];
+
+export type StatusCodeType = keyof typeof StatusCode;
+
+export type MetroPositionType = {
+  rowNum: number;
+  selectedCount: number;
+  subwayId: MetroLineType;
+  subwayNm: MetroLineNameKorType;
+  statnId: string;
+  statnNm: string;
+  trainNo: string;
+  lastRecptnDt: string;
+  recptnDt: string;
+  updnLine: UpDownLineType;
+  statnTid: string;
+  statnTnm: string;
+  trainSttus: TrainStateStatusType;
+  directAt: ExpressStatusType;
+  lstcarAt: LastStatusType;
+};
+export type MetroListResponse = {
+  errorMessage: {
+    status: number;
+    code: StatusCodeType;
+    total: number;
+  };
+  realtimePositionList: MetroPositionType[];
+};
