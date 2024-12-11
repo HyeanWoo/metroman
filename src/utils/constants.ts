dc8c276a7a897076ef82182670b18a22a1c24f54
Hyeanwoo <wkdus7h@naver.com>
2024-12-11 17:35:08 +0900

    feat: add new types for metro-line

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -72,3 +72,42 @@ export const StationList = [
   { id: 1004000455, name: '정왕' },
   { id: 1004000456, name: '오이도' },
 ];
+
+export const UpDownLine = {
+  upAndClockwise: '0',
+  downAndCounterClockwise: '1',
+} as const;
+export const TrainStateStatus = {
+  entered: '0',
+  arrived: '1',
+  departed: '2',
+  preDeparted: '3',
+} as const;
+export const ExpressStatus = {
+  normal: '0',
+  express: '1',
+  limited: '7',
+} as const;
+export const LastStatus = {
+  normal: '0',
+  last: '1',
+} as const;
+
+export const StatusCode = {
+  'INFO-000': '정상 처리되었습니다.',
+  'ERROR-300': '필수 값이 누락되어 있습니다.',
+  'INFO-100': '인증키가 유효하지 않습니다.',
+  'ERROR-301': '파일타입 값이 누락 혹은 유효하지 않습니다.',
+  'ERROR-310': '해당하는 서비스를 찾을 수 없습니다.',
+  'ERROR-331': '요청시작위치 값을 확인하십시오.	',
+  'ERROR-332': '요청종료위치 값을 확인하십시오.',
+  'ERROR-333': '요청위치 값의 타입이 유효하지 않습니다.',
+  'ERROR-334': '요청종료위치 보다 요청시작위치가 더 큽니다.',
+  'ERROR-335':
+    '샘플데이터(샘플키:sample) 는 한번에 최대 5건을 넘을 수 없습니다.',
+  'ERROR-336': '데이터요청은 한번에 최대 1000건을 넘을 수 없습니다.',
+  'ERROR-500': '서버 오류입니다.',
+  'ERROR-600': '데이터베이스 연결 오류입니다.',
+  'ERROR-601': 'SQL 문장 오류 입니다.',
+  'INFO-200': '해당하는 데이터가 없습니다.',
+} as const;
