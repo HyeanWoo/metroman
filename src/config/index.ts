0d694b9f0db11ab9a49ae9041ced1245589dc288
HyeanWoo <46041638+HyeanWoo@users.noreply.github.com>
2024-12-19 18:19:07 +0900

    deploy v0.1.2 (#7)
    * chore: add dotenv & register env files
    
    * chore: add cors origin
    
    * chore: update gitignore
    
    * chore: add metro constants
    
    * feat: create metro position mocking function
    
    * feat: add mock router
    
    * refactor: detach healthCheck router
    
    * chore: add client url on env
    
    * chore: detach option
    
    * chore: add delay function to mocking
    
    * refactor: detach config
    
    * refactor: detach register plugins
    
    * feat: add cache
    
    * feat: add graceful shutdown function
    
    * refactor: move mock to services
    
    * feat: add metrolist route
    
    * chore: update server.ts
    
    * refactor: detach types from constants
    
    * feat: add new types for metro-line
    
    * chore: apply type
    
    * fix: metro line api name
    
    * chore: commented interval
    
    * chore: update package.json
    
    * chore: move @types/node dependency
    
    * chore: add HOST config

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,7 @@ dotenv.config({ path: '.env.local' });
 dotenv.config();
 
 export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const HOST = '0.0.0.0';
 export const PORT = parseInt(process.env.PORT || '8080', 10);
 export const CACHE_TTL = parseInt(process.env.CACHE_TTL || '60', 10); // 초 단위
 export const REFRESH_INTERVAL = parseInt(
